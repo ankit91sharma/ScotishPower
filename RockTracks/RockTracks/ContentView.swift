430d511ddb5d7f0d58c0d2712b179fa1fc3551de
ankit91sharma <34414404+ankit91sharma@users.noreply.github.com>
2025-05-03 19:40:48 +0100

    Project setup
    initial setup for project

---
+++ RockTracks/RockTracks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RockTracks
+//
+//  Created by Ankit Sharma on 03/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RockTracks/RockTracks/RockTracksApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockTracksApp.swift
+//  RockTracks
+//
+//  Created by Ankit Sharma on 03/05/25.
+//
+
+import SwiftUI
+
+@main
+struct RockTracksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
